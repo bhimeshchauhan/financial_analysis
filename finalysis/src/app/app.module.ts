3bf13f09fd7cf08b5b1b727b0beef4732bd71701
Bhimesh Chauhan <bhimeshchauhan@esarcbot.local>
2020-06-17 17:54:19 -0400

    dashboard and nav

--- finalysis/src/app/app.module.ts
+++ finalysis/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
